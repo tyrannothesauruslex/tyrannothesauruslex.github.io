17251498633e631db5237dbb6805236109fba3d7
tyrannothesauruslex <jhnklly.dev@gmail.com>
2014-09-03 09:04:25 -0700

    first commit

---
+++ assets/index.js
@@ -0,0 +1,227 @@
+
+var baseUrl = "http://api.wordnik.com/v4/word.json/";
+var apiKey = "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"; //demo key from developer.wordnik.com
+var path;
+
+var SCHEMAS = {};
+/*SCHEMAS.definitions = array of objects
+definition would be for i++ result[i].text*/
+
+window.onload = function() {
+    document.getElementById("your_word").focus();
+    initAutoThesaurus();
+    $('#your_word').focus();
+};
+
+//$(function() { $('#your_word').focus(); });
+
+function getSynPath() {
+    //console.log( $('#your_word').val() );
+    var word = extractor( $('#your_word').val() );
+    //if (word.length < 2) { return null; }
+    //console.log( word );
+    var res = $('.tt-dataset-foo').find('p')[0];
+    //console.log( '.tt-dataset-foo', $(res).html() );
+    path = baseUrl + word + "/relatedWords?useCanonical=true&relationshipTypes=synonym&limitPerRelationshipType=100&api_key=" + apiKey;
+    //path = baseUrl + word + "/definitions?useCanonical=true&relationshipTypes=synonym&limitPerRelationshipType=100&api_key=" + apiKey;
+    return path;
+}
+
+function getDefPath() {
+    //console.log( $('#your_word').val() );
+    var word = extractor( $('#your_word').val() );
+    //if (word.length < 2) { return null; }
+    console.log( 'getDefPath', word );
+    var res = $('.tt-dataset-foo').find('p')[0];
+    console.log( '.tt-dataset-foo', $(res).html() );
+    path = baseUrl + word + "/definitions?useCanonical=true&relationshipTypes=synonym&limitPerRelationshipType=100&api_key=" + apiKey;
+    return path;
+}
+
+function extractor(query) {
+    var result = /([^ ]+)$/.exec(query);
+    if(result && result[1])
+        return result[1].trim();
+    return '';
+}
+
+function initAutoThesaurus() {
+    /*http://stackoverflow.com/questions/21710289/how-to-use-typeahead-js-0-10-step-by-step-remote-prefetch-local*/
+    var remoteSynHound = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      prefetch: {
+          url: getSynPath(),
+          /*replace: function (response) {
+            console.log('response', response); 
+            return getSynPath();
+          },*/
+          /*filter: function(response) {     
+            console.log('response', response); 
+            return response;
+          }*/
+      },      
+      remote: {
+        url: getSynPath(),
+        replace: function () {
+            return getSynPath();
+        },
+        // the json file contains an array of strings, but the Bloodhound
+        // suggestion engine expects JavaScript objects so this converts all of
+        // those strings
+        filter: function(list) {
+            //return $.map(list, function(country) { return { words: country }; });
+            // list is an array with one element, an object
+            // list[0].words is an array of the words
+            /*console.log(list);
+            console.log(list[0]);
+            console.log(list[0].words);
+            */
+            arr = list[0].words;
+            new_arr = [];
+            //new_list = '';
+            for (var i = 0; i < arr.length; i++) {
+              new_arr.push('<span class="syn">' + arr[i] + '</span>');
+            };
+            //return arr.join('|');
+            //console.log(arr);
+            
+            // put them back together
+            new_list = [{"words": new_arr}];
+            //list[0].words = new_arr;
+            //console.log('new obj',list[0].words);
+            console.log(new_list);
+            return new_list;
+        }
+      }
+    });
+
+  var remoteDefHound = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      prefetch: {
+          url: getDefPath(),
+          replace: function (response) {
+            //console.log('response', response); 
+            return getDefPath();
+          },
+          /*filter: function(response) {     
+            //console.log('response', response); 
+            return response;
+          }*/
+      },      
+      remote: {
+        url: getDefPath(),
+        replace: function () {
+            return getDefPath();
+        },
+        filter: function(response) {
+            //return $.map(list, function(country) { return { words: country }; });
+            // list is an array with one element, an object
+            // list[0].words is an array of the words
+            console.log(response);
+            console.log(response[0]);
+            //console.log(response[0]);
+            return response;
+        }
+      }
+    });
+     
+    remoteSynHound.initialize();
+    remoteDefHound.initialize();
+     
+    /*$('.typeahead').typeahead(null, {
+      name: 'foo',
+      displayKey: 'words',
+      //displayKey: 'text',
+      source: remoteSynHound.ttAdapter(),
+    }); */ 
+
+    $('.typeahead').typeahead(null, 
+        {
+          name: 'foo',
+          displayKey: 'words',
+          //displayKey: 'text',
+          menu: '#idSynonymList',
+          source: remoteSynHound.ttAdapter(),
+          //templates: {
+              /*empty: [
+                '<div class="empty-message">',
+                '',
+                '</div>'
+              ].join('\n'),*/
+              //suggestion: Handlebars.compile('<p><span class="synonym">{{words}}</span> |</p>')
+              //suggestion: Handlebars.compile(['<span class="synonym">{{words}}</span>'].join('|'))
+              /*suggestion: [
+                '<span class="synonym">','displayKey','</span>'
+              ].join(' ')*/
+          //}          
+        },
+        {
+          name: 'foo2',
+          displayKey: 'text',
+          menu: '#idDefList',
+          source: remoteDefHound.ttAdapter(),
+        }
+    );
+
+    //$(document).on('click', '.tt-dropdown-menu,.tt-dataset-foo,.twitter-typeahead,.tt-suggestions,.tt-suggestion p', function(e) {
+    $(document).on("click.tt", ".tt-suggestion", function(e) {
+        console.log('click.tt, .tt-suggestion', $(this).html() );
+        console.log('click.tt, .tt-suggestion', $(this) );
+        console.log('.paper html', $('.paper').html() );
+        //$(this).show();
+        e.stopPropagation();
+        e.preventDefault();
+    })
+    /*
+    $(document).on('click', '.tt-dataset-foo', function(e) {
+        console.log('click, .tt-dataset-foo', $(this).html() );
+        e.preventDefault();
+    })
+
+    $('*').click(function () {
+        myFunction(this);
+    });
+
+    function myFunction(element) {
+        console.log(element);
+        console.log(element.id);
+        console.log($(element).attr("class"));
+    }*/
+
+    /*$('.tt-dataset-foo').on('mouseenter', '.tt-dataset-foo', function(e) {
+        console.log('mouseenter, .tt-dataset-foo');
+        e.preventDefault();
+    })*/
+    //.on('typeahead:closed', onClosed)
+}
+
+/*function onOpened() {
+    console.log( '.tt-dataset-foo', $('.tt-dataset-foo')[0] );
+}
+function onClosed($e, datum) {
+    console.log('onclosed');
+    //NG: if (!$(".tt-dropdown-menu").is(":visible")) {
+    //    $(".tt-dropdown-menu").show();
+    //}
+}*/
+    /* See also 
+    http://stackoverflow.com/questions/21726756/not-able-to-make-twitter-typeahead-0-10-with-bloodhound-with-prefetch-to-work 
+    http://fusiongrokker.com/post/heavily-customizing-a-bootstrap-typeahead
+    http://brianreavis.github.io/selectize.js/
+    */
+
+
+/*
+var bhResults = new Bloodhound({
+    datumTokenizer: function (d) { return Bloodhound.tokenizers.whitespace(d.num); },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: []
+});
+*/
+/*function typeaheadResults(query, callback) {
+    //console.log(query);
+}*/
+
+
